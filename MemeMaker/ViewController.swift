7999bb7d68fc6a3dd2a258f85ee00af3932ea0fb
Belinda Vennam <beemarie88@gmail.com>
2016-04-13 11:15:11 -0400

    updating text attributes

--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    var topIsEdited:Bool?
+    var bottomIsEdited:Bool?
+    
+    //dictionary of text attributes
+    let textAttributes = [
+        NSStrokeColorAttributeName: UIColor.blackColor(),
+        NSFontAttributeName:  UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSStrokeWidthAttributeName: -1.0,
+    ]
+    
+    
     var imagePicker: UIImagePickerController?
     @IBOutlet weak var theImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.topTextField.delegate = self
+        topTextField.defaultTextAttributes = textAttributes
+        topTextField.textAlignment = NSTextAlignment.Center
+        self.bottomTextField.delegate = self
+        bottomTextField.defaultTextAttributes = textAttributes
+        bottomTextField.textAlignment = NSTextAlignment.Center
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+        topIsEdited = false
+        bottomIsEdited = false
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -34,6 +58,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         //after selecting image dismiss the view controller
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if (textField == topTextField && topIsEdited == false) {
+            textField.text = ""
+            topIsEdited = true
+        } else if (textField == bottomTextField && bottomIsEdited == false) {
+            textField.text = ""
+            bottomIsEdited = true
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
 
 }
 
